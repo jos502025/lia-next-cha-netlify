c8559fc52f13ecfa877556652b4f314b6f831984
jos502025 <jcvazquezr@live.com.mx>
2025-09-14 18:10:07 -0600

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <main style={{display: "flex", justifyContent: "center", alignItems: "center", height: "100vh"}}>
+      <h1>🚀 ¡LÍA desplegado con éxito en Netlify!</h1>
+    </main>
+  )
+}
